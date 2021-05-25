fe7c878494629ea2058cf6edbe33c6b635f5a9fe
PauloMacedoEI <paulomacedo@ipvc.pt>
2021-05-25 17:33:55 +0100

    Initial commit to development

--- app/src/main/java/estg/cm/letscook/MainActivity.kt
+++ app/src/main/java/estg/cm/letscook/MainActivity.kt
@@ -2,7 +2,7 @@ package estg.cm.letscook
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+//
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
