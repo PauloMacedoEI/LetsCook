e3aed18827cdfcf0ebe7301e3eff606320fa6d8e
PauloMacedoEI <paulomacedo@ipvc.pt>
2021-06-07 15:22:01 +0100

    Information layout updated (Ingredients and Steps list)

--- app/src/main/java/estg/cm/letscook/RecipeInformation.kt
+++ app/src/main/java/estg/cm/letscook/RecipeInformation.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.ImageView
+import android.widget.TextClock
 import android.widget.TextView
 import androidx.lifecycle.ViewModel
 import com.makeramen.roundedimageview.RoundedTransformationBuilder
@@ -18,6 +19,8 @@ class RecipeInformation : AppCompatActivity() {
     private lateinit var startButton : ImageView
     private lateinit var servings: TextView
     private lateinit var duration: TextView
+    private lateinit var ingredients: TextView
+    private lateinit var steps: TextView
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,6 +36,9 @@ class RecipeInformation : AppCompatActivity() {
         startButton = findViewById(R.id.item_start_icon_information)
         duration = findViewById(R.id.item_duration_information)
         servings = findViewById(R.id.item_servings_information)
+        ingredients = findViewById(R.id.item_ingredients)
+        steps = findViewById(R.id.item_steps)
+
 
         startButton.setOnClickListener {
             val recipe2 =  arrayListOf<Recipe>()
@@ -58,9 +64,19 @@ class RecipeInformation : AppCompatActivity() {
             val minutes = recipe[0].duration!! % 60
             duration.text = hours.toString().plus(getString(R.string.recipe_information_hours)).plus(" ").plus(minutes.toString()).plus(getString(R.string.recipe_information_minutes))
         } else {
-            duration.text = recipe?.get(0)?.duration.toString().plus(getString(R.string.recipe_information_minutes))
+            duration.text = recipe[0]?.duration.toString().plus(getString(R.string.recipe_information_minutes))
         }
-        servings.text = recipe?.get(0)?.servings.toString().plus(" ").plus(getString(R.string.recipe_information_servings))
+        servings.text = recipe[0]?.servings.toString().plus(" ").plus(getString(R.string.recipe_information_servings))
+        for (ingredient in recipe[0].ingredients){
+            ingredients.text = ingredients.text.toString().plus(ingredient.name.plus(" - ").plus(ingredient.quantity).plus("\n"))
+        }
+
+        var number = 1
+        for (step in recipe[0].steps){
+            steps.text = steps.text.toString().plus("$number - ").plus(step.description.plus("\n"))
+            number++
+        }
+
 
 
 
